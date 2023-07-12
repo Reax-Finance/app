bcc9e045209eb691fc59d4c4d8272fb926fafb90
prasad-kumkar <pk111_@live.com>
2023-07-12 13:22:18 +0530

    build fix

---
+++ components/account/positions.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Positions() {
+  return (
+    <Box>
+
+        <Box>
+            <Heading size={'md'}>Lending Positions</Heading>
+        </Box>
+    
+    </Box>
+  )
+}
---
+++ components/dashboard/MinimalPosition.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MinimalPosition() {
+  return (
+    <div>MinimalPosition</div>
+  )
+}
