2e17a6e0cc79bfb3aa20ed725e00f27d494a8448
prasad-kumkar <pk111_@live.com>
2023-08-02 21:12:15 +0530

    ui fixes

--- components/swap/SwapLayout.tsx
+++ components/swap/SwapLayout.tsx
@@ -321,7 +321,7 @@ export default function SwapLayout({
                     </motion.div>
                 </Box>
                 </Box>}
-                <Box mt={isOpen? 0 : 3} mb={5} className={validate().valid ? "primaryButton" : "disabledPrimaryButton"}>
+                <Box mt={isOpen? 0 : valid ? 3 : 5} mb={5} className={validate().valid ? "primaryButton" : "disabledPrimaryButton"}>
                 <Button
                     size="lg"
                     fontSize={"xl"}
