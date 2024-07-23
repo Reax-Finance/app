63518102c2c6fae46d583787e18be2c276b376d8
Ghastly2001 <divyanshverma18may@gmail.com>
2024-07-23 22:15:38 +0530

    rainbowkit auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,10 @@ import "../styles/rounded-light.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
 import {
-	Chain,
-	RainbowKitProvider,
-	connectorsForWallets,
-	getDefaultConfig,
+  Chain,
+  RainbowKitProvider,
+  connectorsForWallets,
+  getDefaultConfig,
 } from "@rainbow-me/rainbowkit";
 import { Box, ChakraProvider, Flex } from "@chakra-ui/react";
 import Index from "./_index";
@@ -19,43 +19,56 @@ import { AppDataProvider } from "../components/context/AppDataProvider";
 import { theme } from "../styles/theme";
 import { supportedChains } from "../src/const";
 import {
-	BalanceContext,
-	BalanceContextProvider,
+  BalanceContext,
+  BalanceContextProvider,
 } from "../components/context/BalanceProvider";
 import { PriceContextProvider } from "../components/context/PriceContext";
 import "@rainbow-me/rainbowkit/styles.css";
 import { WagmiProvider } from "wagmi";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+import {
+  RainbowKitSiweNextAuthProvider,
+  GetSiweMessageOptions,
+} from "@rainbow-me/rainbowkit-siwe-next-auth";
+
 const config = getDefaultConfig({
-	appName: "My RainbowKit App",
-	projectId: "YOUR_PROJECT_ID",
-	chains: supportedChains as any,
-	ssr: true, // If your dApp uses server side rendering (SSR)
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: supportedChains as any,
+  ssr: true, // If your dApp uses server side rendering (SSR)
 });
 
 const queryClient = new QueryClient();
 
+const getSiweMessageOptions: GetSiweMessageOptions = () => ({
+  statement: "Sign in to my RainbowKit app",
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<ChakraProvider theme={theme}>
-			<WagmiProvider config={config}>
-				<QueryClientProvider client={queryClient}>
-					<RainbowKitProvider>
-						<AppDataProvider>
-							<BalanceContextProvider>
-								<PriceContextProvider>
-									<Index>
-										<Component {...pageProps} />
-									</Index>
-								</PriceContextProvider>
-							</BalanceContextProvider>
-						</AppDataProvider>
-					</RainbowKitProvider>
-				</QueryClientProvider>
-			</WagmiProvider>
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider theme={theme}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitSiweNextAuthProvider
+            getSiweMessageOptions={getSiweMessageOptions}
+          >
+            <RainbowKitProvider>
+              <AppDataProvider>
+                <BalanceContextProvider>
+                  <PriceContextProvider>
+                    <Index>
+                      <Component {...pageProps} />
+                    </Index>
+                  </PriceContextProvider>
+                </BalanceContextProvider>
+              </AppDataProvider>
+            </RainbowKitProvider>
+          </RainbowKitSiweNextAuthProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
