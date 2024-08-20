8473df358a0806d8345e3d999481c863a1cefd6a
prasad-kumkar <pk111_@live.com>
2024-08-21 02:20:17 +0530

    twt-activity

--- src/const.ts
+++ src/const.ts
@@ -178,8 +178,8 @@ export const TWITTER_SCOPE = [
   // "follows.write",
 ].join(" ");
 
-export const X_ACCOUNT_AGE_MIN = 0 * 24 * 60 * 60 * 1000;
-export const X_ACCOUNT_FOLLOWERS_MIN = 0;
+export const X_ACCOUNT_AGE_MIN = 3 * 24 * 60 * 60 * 1000;
+export const X_ACCOUNT_FOLLOWERS_MIN = 10;
 
 export const breakpoints = {
   base: "0em", // 0px
